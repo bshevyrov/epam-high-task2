813d246ad6b2901344101f56dee03ee0723ebc01
bohdan <bshevyrov@gmail.com>
2024-08-23 06:55:19 +0300

    initial commit

---
+++ src/test/java/ua/com/company/EpamHighTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpamHighTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
